88a1922b0c5668a62060408cde066b387be436da
Lenovo <Lenovo@DESKTOP-ESKVQ4N>
2020-01-29 13:45:45 +0530

    pos n neg

---
+++ epam_practice/src/epam_practice/E21.java
@@ -0,0 +1,15 @@
+package epam_practice;
+class E21
+{
+public static void main(String args[])
+{
+int i=Integer.parseInt(args[0]);
+if(i>0)
+System.out.println(i+"is positive");
+else if(i<0)
+System.out.println(i+"is negative");
+else
+System.out.println(i+"is neither positive nor negative");
+}
+
+}
